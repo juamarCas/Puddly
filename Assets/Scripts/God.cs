38de7299176df3b19a570220c55bdbd0df4fb1b8
Juan Martin <juanmartincas@gmail.com>
2020-09-30 12:37:12 -0500

    Box selection

--- Assets/Scripts/God.cs
+++ Assets/Scripts/God.cs
@@ -1,14 +1,12 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.EventSystems; 
 using UnityEngine;
 
-
 public class God : MonoBehaviour
 {
 
-
+    private Camera cam; 
     //Selected tile and ghostBuilding
     private GameObject hoveredTile;
     private GameObject ghostBuilding;
@@ -21,20 +19,26 @@ public class God : MonoBehaviour
 
 
     //is selecting place to construct? 
-    private bool isConstructing; 
+    private bool isConstructing;
 
     // saves the gameobject you interact with
-    [SerializeField] GameObject unit;
+    [SerializeField] List<Unit> units = new List<Unit>();
+
+    // units you have created
+    [SerializeField] List<Unit> createdUnits = new List<Unit>(); 
     
 
     //by now, not important, is for UI check
     private int FingerID = -1; 
 
-    private bool buildCooled = true; 
-    
+    private bool buildCooled = true; /*by now, this varuable is not important, used to cool dow a little bit the constructions*/
+
+    //variables for the selection box
+    private Vector2 startPos;
+    private Vector2 endPos; 
     
     // states 
-    public enum States {Free = 0, HasVillager = 1};
+    public enum States {Free = 0, HasVillager = 1, HasWarrior = 2, HasMixed = 3, HasBuilding = 4 }; //mixed means has warriors and villagers
     public enum BuildingSel {Nothing = -1,House = 0}
 
     [Header("Components")]
@@ -54,7 +58,9 @@ public class God : MonoBehaviour
     public List<GameObject> Buildings;
 
     [Header("UI Elements")]
-    [SerializeField] private CanvasManager canvas; 
+    [SerializeField] private CanvasManager canvas;
+    public RectTransform selectionBox;
+
 
     [Header("Masks")]
     [SerializeField] private LayerMask tileMask;
@@ -65,6 +71,7 @@ public class God : MonoBehaviour
     public BuildingSel buildSel; 
     void Start()
     {
+        cam = Camera.main; 
         state = States.Free;
         buildSel = BuildingSel.Nothing; 
         #if !UNITY_EDITOR
@@ -85,27 +92,33 @@ public class God : MonoBehaviour
         {
             if(buildSel != BuildingSel.Nothing)
                 SetFree(); 
-            if(unit != null)
-            {
+            if(units.Count > 0)
                 UnSelectUnit(); 
-            }
+            
         }
-
-        if (Input.GetMouseButtonDown(0))
+        #region one one unit selection
+        if (Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.LeftShift))
+        {
+            GameObject unit = SelectObject(selectableUnitMask);
+            units.Add(unit.GetComponent<Unit>());
+            unit = null; 
+        } else if (Input.GetMouseButtonDown(0))
         {
             //select a unit this includes: buildings, villagers, warriors
-                         
+            startPos = Input.mousePosition;             
             
             if (!isConstructing)
             {
                 //selecting a unit   
-                unit = SelectObject(selectableUnitMask);
+                GameObject unit = SelectObject(selectableUnitMask);
                 if (unit != null)
                 {
+                    units.Clear(); 
                     canvas.nameText.text = unit.GetComponent<Unit>().unitName;
                     if (unit.gameObject.tag == "Villager")
-                    {
-                        state = States.HasVillager;
+                    {               
+                        state = States.HasVillager;                      
+                        units.Add(unit.GetComponent<Unit>()); 
                     }
                     else
                     {
@@ -113,10 +126,41 @@ public class God : MonoBehaviour
                     }                  
                 }           
             }
-        }else if (Input.GetMouseButtonDown(1) && state == States.HasVillager)
+        }else if (Input.GetMouseButtonDown(1))
         {
-
+            //make an action with villager, warrior or building
+            if(state == States.HasVillager)
+            {
+                //get villager properties
+                if(units.Count > 0)
+                {
+                    for(int i = 0; i < units.Count; i++)
+                    {
+                        if(units[i].transform.tag == "Villager")
+                        {
+                            units[i].gameObject.GetComponent<Puddly>().MoveDestination(DestinationPoint()); 
+                        }
+                    }
+                }
+            }
         }
+        #endregion
+
+        #region multiple selectiom
+        //end box selection
+        if (Input.GetMouseButtonUp(0))
+        {
+            ReleaseSelectionBox(); 
+        }
+
+        //start box selection
+        if (Input.GetMouseButton(0))
+        {
+            UpdateSelectionBox(Input.mousePosition); 
+        }
+        #endregion
+
+
         if (buildSel == BuildingSel.House)
         {
             hoverTile(); 
@@ -151,7 +195,7 @@ public class God : MonoBehaviour
             }
         }
     }
-
+   
  
     GameObject SelectObject(LayerMask desiredLayer)
     {
@@ -173,12 +217,27 @@ public class God : MonoBehaviour
         return null; 
     }
 
+    Vector3 DestinationPoint()
+    {
+        
+        if(!EventSystem.current.IsPointerOverGameObject()){
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, tileMask))
+            {             
+                if (hit.transform.gameObject.GetComponent<Tiles>().state == Tiles.States.free)                   
+                    return hit.transform.position;
+            }
+        }
+        return this.transform.position; 
+    }
+
     void UnSelectUnit()
     {
-        if (unit != null)
+        if (units.Count > 0)
         {
-            canvas.nameText.text = " ";
-            unit = null;
+            canvas.nameText.text = "";
+            units.Clear(); 
             state = States.Free; 
         }
        
@@ -193,17 +252,50 @@ public class God : MonoBehaviour
         Building_ghost[selectedBuilding].transform.position = this.transform.position;
         isConstructing = false; 
     }
+
+    void UpdateSelectionBox(Vector2 currentMousePos)
+    {
+        if (!selectionBox.gameObject.activeInHierarchy)
+            selectionBox.gameObject.SetActive(true);
+
+        float width = currentMousePos.x - startPos.x;
+        float height = currentMousePos.y - startPos.y;
+
+        selectionBox.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
+        selectionBox.anchoredPosition = startPos +  new Vector2(width / 2 , height / 2 ); 
+    }
+
+    void ReleaseSelectionBox()
+    {
+        selectionBox.gameObject.SetActive(false);
+
+        Vector2 min = selectionBox.anchoredPosition - (selectionBox.sizeDelta / 2);
+        Vector2 max = selectionBox.anchoredPosition + (selectionBox.sizeDelta / 2); 
+
+        foreach(Unit puddly in createdUnits)
+        {
+            Vector3 screenPos = cam.WorldToScreenPoint(puddly.transform.position); 
+
+            if(screenPos.x > min.x && screenPos.x < max.x && screenPos.y > min.y && screenPos.y < max.y)
+            {
+                units.Add(puddly);          
+            }
+        }
+
+        if(units.Count > 0)
+        {
+            state = States.HasVillager; 
+        }
+    }
     #endregion
 
     void SetBuilding()
     {
-        //build the vuilding
+        //build the building
         Instantiate(Buildings[selectedBuilding], Building_ghost[selectedBuilding].transform.position, Building_ghost[selectedBuilding].transform.rotation);
         Building_ghost[selectedBuilding].transform.position = this.transform.position;
         Building_ghost[selectedBuilding].SetActive(false); 
     }
-
-    
 
     private IEnumerator BuildingCoolDown()
     {
---
+++ Assets/Scripts/Puddly.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; 
+
+public class Puddly : Unit
+{
+    private NavMeshAgent agent; 
+
+    [Header("Attributes")]
+    public float velocity;
+    public float angularSpeed; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = velocity;
+        agent.angularSpeed = angularSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveDestination(Vector3 dest)
+    {
+        agent.SetDestination(dest); 
+    }
+}
